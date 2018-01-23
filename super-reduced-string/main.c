75d39dbd29ab7f7b05c0c94d82cd3cc03051ef4c
Ben Iofel <iofelben@gmail.com>
2018-01-22 20:15:51 -0500

    remove comments

--- super-reduced-string/main.c
+++ super-reduced-string/main.c
@@ -4,13 +4,12 @@
 
 void reduce(char *str) {
     for (int i = 0; str[i+1] != '\0'; ++i) {
-        printf("checking if str[%d] = '%c'  == str[%d] = '%c'\n", i, str[i], i+1, str[i+1]);
+        //printf("checking if str[%d] = '%c'  == str[%d] = '%c'\n", i, str[i], i+1, str[i+1]);
         if (str[i] == str[i+1]) {
-            printf("yes, memmove(%d, %d, %d)\n", i+1, i+3, strlen(&str[i+3])+1);
+            //printf("yes, memmove(%d, %d, %d)\n", i+1, i+3, strlen(&str[i+3])+1);
             memmove(&str[i], &str[i+2], strlen(&str[i+2])+1);
-            printf("new string: %s\n", str);
+            //printf("new string: %s\n", str);
             i -= 2;
-            //if (i < 0) i = 0;
         }
     }
 }
