a5ba08fa92422f1b9dfed0ee72bc232b24bcc531
Ben Iofel <iofelben@gmail.com>
2018-01-29 18:58:38 -0500

    profile

--- prime-factors/main.c
+++ prime-factors/main.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <time.h>
+
+void timespec_diff(struct timespec *start, struct timespec *stop,
+                   struct timespec *result)
+{
+    if ((stop->tv_nsec - start->tv_nsec) < 0) {
+        result->tv_sec = stop->tv_sec - start->tv_sec - 1;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
+    } else {
+        result->tv_sec = stop->tv_sec - start->tv_sec;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec;
+    }
+
+    return;
+}
 
 int main() {
+    struct timespec start, end; 
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     long n = 600851475143;
+    //long n = 13195;
     long res = 1;
-    //int n = 13195;
 
     for (int p = 2; p < n/2; ++p) {
         bool isPrime = true;
 
-        for (int i = 3; i < sqrt(p); ++i)
+        for (int i = 3; i < sqrt(p); i += 22)
             if (p % i == 0) {
                 isPrime = false;
                 break;
@@ -25,4 +43,11 @@ int main() {
         if (res == n)
             break;
     }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    struct timespec diff;
+    timespec_diff(&start, &end, &diff);
+
+    printf("%ld sec, %f msec\n", diff.tv_sec, ((double)diff.tv_nsec)/1000/1000);
 }
