586ab6377fe04c7255f53d298b52a5e6b1ccce3c
Ben Iofel <iofelben@gmail.com>
2018-02-12 18:49:55 -0500

    is-bst

---
+++ is-bst/main.py
@@ -0,0 +1,4 @@
+def check_binary_search_tree_(root):
+    def check(root, minx = -2147483647, maxx = 2147483647):
+        return root.data > minx and root.data < maxx and (root.left == None or check(root.left, minx, root.data)) and (root.right == None or check(root.right, root.data, maxx))
+    return check(root)
