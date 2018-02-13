7a5435d3df7bf006e660918cce86d3c907f8a14d
Ben Iofel <iofelben@gmail.com>
2018-02-12 20:30:44 -0500

    better

--- stack-max/main.c
+++ stack-max/main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 #define PUSH 1
 #define DELETE 2
 #define PRMAX 3
-
-#define MAX(a, b) (a > b ? a : b)
 
 int getint() {
   int x;
@@ -16,12 +13,12 @@ int getint() {
   return x;
 }
 
-void frame(int max, int val) {
-  while (1) {
+void frame(int max) {
+  for (;;) {
     int op = getint();
     if (op == PUSH) {
       int x = getint();
-      frame(MAX(x, max), x);
+      frame(x > max ? x : max);
     } else if (op == DELETE) {
       return;
     } else if (op == PRMAX) {
@@ -32,13 +29,5 @@ void frame(int max, int val) {
 
 int main() {
   getint();
-
-  while (1) {
-    int op = getint();
-    assert(op == PUSH);
-    if (op == PUSH) {
-      int x = getint();
-      frame(x, x);
-    }
-  }
+  frame(-1);
 }
