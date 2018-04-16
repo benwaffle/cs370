1db64e7afecf960b3a9791c2f51c9ffce6320cc5
Ben Iofel <iofelben@gmail.com>
2018-04-16 19:46:16 -0400

    faster

--- board-cutting/main.py
+++ board-cutting/main.py
@@ -15,30 +15,44 @@ def index(arr, elem):
         return -1
 
 def boardCutting(cost_y, cost_x):
-    cost_y = list(map(lambda a: (a, 1), cost_y))
-    cost_x = list(map(lambda a: (a, 1), cost_x))
+    cost_y = list(map(lambda a: [a, 1], cost_y))
+    cost_x = list(map(lambda a: [a, 1], cost_x))
 
     def done():
-        return sum(map(fst, cost_y + cost_x)) == 0
+        for c in cost_y:
+            if c[0] != 0:
+                return False
+        for c in cost_x:
+            if c[0] != 0:
+                return False
+        return True
 
     cost = 0
     while not done():
         #print('==============')
         #print('Costs:', cost_y, cost_x)
 
-        largestCost = max(map(fst, cost_y + cost_x))
+        largestCost = -1
+        for c in cost_y:
+            if c[0] > largestCost:
+                largestCost = c[0]
+        for c in cost_x:
+            if c[0] > largestCost:
+                largestCost = c[0]
 
         #print('largest cost', largestCost)
 
-        smallestCutY = index(list(map(fst, cost_y)), largestCost)
+        smallestCutY = -1
         for i, c in enumerate(cost_y):
-            if c[0] == largestCost and c[1] < cost_y[smallestCutY][1]:
-                smallestCutY = i
+            if c[0] == largestCost:
+                if smallestCutY == -1 or c[1] < cost_y[smallestCutY][1]:
+                    smallestCutY = i
 
-        smallestCutX = index(list(map(fst, cost_x)), largestCost)
+        smallestCutX = -1
         for i, c in enumerate(cost_x):
-            if c[0] == largestCost and c[1] < cost_x[smallestCutX][1]:
-                smallestCutX = i
+            if c[0] == largestCost:
+                if smallestCutX == -1 or c[1] < cost_y[smallestCutX][1]:
+                    smallestCutX = i
 
         #print('smallest y cuts', smallestCutY)
         #print('smallest x cuts', smallestCutX)
@@ -49,15 +63,20 @@ def boardCutting(cost_y, cost_x):
         if smallestCutX == -1 or (smallestCutY != -1 and cost_y[smallestCutY][1] < cost_x[smallestCutX][1]):
             #print('CUTTING ACROSS Y', smallestCutY, 'cost = ', largestCost)
             cost += cost_y[smallestCutY][1] * largestCost
-            cost_x = list(map(lambda a: (a[0], a[1]+1), cost_x))
-            cost_y[smallestCutY] = (0,0)
+            #cost_x = list(map(lambda a: (a[0], a[1]+1), cost_x))
+            for c in cost_x:
+                c[1] += 1
+
+            cost_y[smallestCutY][0] = 0
         else:
             #print('CUTTING ACROSS X', smallestCutX, 'cost = ', largestCost)
             cost += cost_x[smallestCutX][1] * largestCost
-            cost_y = list(map(lambda a: (a[0], a[1]+1), cost_y))
-            cost_x[smallestCutX] = (0,0)
+            #cost_y = list(map(lambda a: (a[0], a[1]+1), cost_y))
+            for c in cost_y:
+                c[1] += 1
+            cost_x[smallestCutX][0] = 0
 
-    return cost
+    return cost % (10**9 + 7)
 
 if __name__ == "__main__":
     q = int(input().strip())
