b6dca44698ce6ae75c5dda3eb16e206be702308b
Ben Iofel <iofelben@gmail.com>
2018-04-16 20:12:09 -0400

    faster

--- board-cutting/main.py
+++ board-cutting/main.py
@@ -15,66 +15,66 @@ def index(arr, elem):
         return -1
 
 def boardCutting(cost_y, cost_x):
-    cost_y = list(map(lambda a: [a, 1], cost_y))
-    cost_x = list(map(lambda a: [a, 1], cost_x))
+    cuts_y = [1]*len(cost_y)
+    cost_y.sort(reverse=True)
+    cuts_x = [1]*len(cost_x)
+    cost_x.sort(reverse=True)
 
-    def done():
-        for c in cost_y:
-            if c[0] != 0:
-                return False
-        for c in cost_x:
-            if c[0] != 0:
-                return False
-        return True
-
+    cuts_needed = len(cost_x) + len(cost_y)
+    iters = 0
     cost = 0
-    while not done():
+    while not iters == cuts_needed:
+        iters += 1
         #print('==============')
         #print('Costs:', cost_y, cost_x)
 
-        largestCost = -1
-        for c in cost_y:
-            if c[0] > largestCost:
-                largestCost = c[0]
-        for c in cost_x:
-            if c[0] > largestCost:
-                largestCost = c[0]
+        if len(cost_x) == 0:
+            largestCost = cost_y[0]
+        elif len(cost_y) == 0:
+            largestCost = cost_x[0]
+        else:
+            largestCost = max(cost_x[0], cost_y[0])
 
         #print('largest cost', largestCost)
 
         smallestCutY = -1
         for i, c in enumerate(cost_y):
-            if c[0] == largestCost:
-                if smallestCutY == -1 or c[1] < cost_y[smallestCutY][1]:
-                    smallestCutY = i
+            if c != largestCost:
+                break
+            if smallestCutY == -1 or cuts_y[i] < cuts_y[smallestCutY]:
+                smallestCutY = i
 
         smallestCutX = -1
         for i, c in enumerate(cost_x):
-            if c[0] == largestCost:
-                if smallestCutX == -1 or c[1] < cost_y[smallestCutX][1]:
-                    smallestCutX = i
+            if c != largestCost:
+                break
+            if smallestCutX == -1 or cuts_x[i] < cuts_x[smallestCutX]:
+                smallestCutX = i
 
-        #print('smallest y cuts', smallestCutY)
-        #print('smallest x cuts', smallestCutX)
+        #if smallestCutY != -1:
+        #    print('smallest y cuts', cuts_y[smallestCutY], 'piece')
+        #if smallestCutX != -1:
+        #    print('smallest x cuts', cuts_x[smallestCutX], 'piece')
 
-        assert smallestCutY == -1 or cost_y[smallestCutY][0] == largestCost
-        assert smallestCutX == -1 or cost_x[smallestCutX][0] == largestCost
+        assert smallestCutY == -1 or cost_y[smallestCutY] == largestCost
+        assert smallestCutX == -1 or cost_x[smallestCutX] == largestCost
 
-        if smallestCutX == -1 or (smallestCutY != -1 and cost_y[smallestCutY][1] < cost_x[smallestCutX][1]):
+        if smallestCutX == -1 or (smallestCutY != -1 and cuts_y[smallestCutY] < cuts_x[smallestCutX]):
             #print('CUTTING ACROSS Y', smallestCutY, 'cost = ', largestCost)
-            cost += cost_y[smallestCutY][1] * largestCost
-            #cost_x = list(map(lambda a: (a[0], a[1]+1), cost_x))
-            for c in cost_x:
-                c[1] += 1
+            cost += cuts_y[smallestCutY] * largestCost
+            for i in range(len(cuts_x)):
+                cuts_x[i] += 1
 
-            cost_y[smallestCutY][0] = 0
+            cost_y.pop(0)
+            cuts_y.pop(0)
         else:
             #print('CUTTING ACROSS X', smallestCutX, 'cost = ', largestCost)
-            cost += cost_x[smallestCutX][1] * largestCost
-            #cost_y = list(map(lambda a: (a[0], a[1]+1), cost_y))
-            for c in cost_y:
-                c[1] += 1
-            cost_x[smallestCutX][0] = 0
+            cost += cuts_x[smallestCutX] * largestCost
+            for i in range(len(cuts_y)):
+                cuts_y[i] += 1
+
+            cost_x.pop(0)
+            cuts_x.pop(0)
 
     return cost % (10**9 + 7)
 
